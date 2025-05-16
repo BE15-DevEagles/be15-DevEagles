09a169bcce5effa2960142f9d2580a1b9bbbf4da
hwan1023 <qpzmrlaxoghks@naver.com>
2025-05-16 15:50:00 +0900

    branch ruleset test

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/common/test.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/common/test.java
@@ -1,3 +1,7 @@
 package com.deveagles.be15_deveagles_be.common;
 
-public class test {}
+public class test {
+    public static void main(String[] args) {
+        System.out.println("ASDFASDFASDFASDFSDFASdf");
+    }
+}
