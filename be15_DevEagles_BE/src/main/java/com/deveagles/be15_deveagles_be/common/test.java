41f9f9879d4bc704df35581e7fc5fa5c1e3bcdaf
gyeongmin03 <gyeongmin03@naver.com>
2025-05-16 15:53:16 +0900

    branch rule test

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/common/test.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/common/test.java
@@ -2,6 +2,6 @@ package com.deveagles.be15_deveagles_be.common;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("ASDFASDFASDFASDFSDFASdf");
+        System.out.println("ASDFASDFASDFASDFSDFASdfddgcdfxfdxfsdxdfxdfxdfxdf");
     }
 }
