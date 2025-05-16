4026464d9fba5aac00599188f5424afe064c1221
Yangha Park <64etuor@gmail.com>
2025-05-16 18:52:20 +0900

    chore:의존성 문제 해결

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/common/exception/GlobalExceptionHandler.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.deveagles.be15_deveagles_be.common.exception;
 
-import com.hanhwa_tae.cqrs.common.dto.ApiResponse;
+import com.deveagles.be15_deveagles_be.common.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,33 +10,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ApiResponse<Void>> handleBusinessException(BusinessException e) {
-        ErrorCode errorCode = e.getErrorCode();
-        ApiResponse<Void> response
-                = ApiResponse.failure(errorCode.getCode(), errorCode.getMessage());
-        return new ResponseEntity<>(response, errorCode.getHttpStatus());
+  @ExceptionHandler(BusinessException.class)
+  public ResponseEntity<ApiResponse<Void>> handleBusinessException(BusinessException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    ApiResponse<Void> response = ApiResponse.failure(errorCode.getCode(), errorCode.getMessage());
+    return new ResponseEntity<>(response, errorCode.getHttpStatus());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ApiResponse<Void>> handleValidationException(
+      MethodArgumentNotValidException e) {
+
+    ErrorCode errorCode = ErrorCode.VALIDATION_ERROR;
+    StringBuilder errorMessage = new StringBuilder(errorCode.getMessage());
+    for (FieldError error : e.getBindingResult().getFieldErrors()) {
+      errorMessage.append(String.format("[%s : %s]", error.getField(), error.getDefaultMessage()));
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException e) {
+    ApiResponse<Void> response = ApiResponse.failure(errorCode.getCode(), errorMessage.toString());
+    return new ResponseEntity<>(response, errorCode.getHttpStatus());
+  }
 
-        ErrorCode errorCode = ErrorCode.VALIDATION_ERROR;
-        StringBuilder errorMessage = new StringBuilder(errorCode.getMessage());
-        for(FieldError error : e.getBindingResult().getFieldErrors()) {
-            errorMessage.append(String.format("[%s : %s]", error.getField(), error.getDefaultMessage()));
-        }
-
-        ApiResponse<Void> response
-                = ApiResponse.failure(errorCode.getCode(), errorMessage.toString());
-        return new ResponseEntity<>(response, errorCode.getHttpStatus());
-    }
-
-   /* @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<Void>> handleException() {
-        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
-        ApiResponse<Void> response
-                = ApiResponse.failure(errorCode.getCode(), errorCode.getMessage());
-        return new ResponseEntity<>(response, errorCode.getHttpStatus());
-    }*/
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ApiResponse<Void>> handleException() {
+    ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+    ApiResponse<Void> response = ApiResponse.failure(errorCode.getCode(), errorCode.getMessage());
+    return new ResponseEntity<>(response, errorCode.getHttpStatus());
+  }
 }
