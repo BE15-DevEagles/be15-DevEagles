8f900384bfc49e17c355bf9817eaff1af4f97575
Yangha Park <64etuor@gmail.com>
2025-05-16 11:01:57 +0900

    chore:script logs test

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/chat/test.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/chat/test.java
@@ -3,6 +3,6 @@ package com.deveagles.be15_deveagles_be.features.chat;
 public class test {
 
   public static void main(String[] args) {
-    System.out.println(     "spotless test!"    );
+    System.out.println(  "spotless test!"    );
   }
 }
