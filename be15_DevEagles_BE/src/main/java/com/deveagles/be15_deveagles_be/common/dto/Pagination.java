c1af9f78c07e23acfc515d5867341a15d2040211
Yangha Park <64etuor@gmail.com>
2025-05-16 18:56:18 +0900

    chore:의존성 문제 해결

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/common/dto/Pagination.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/common/dto/Pagination.java
@@ -1,13 +1,12 @@
 package com.deveagles.be15_deveagles_be.common.dto;
 
-
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class Pagination {
-    private final int currentPage;
-    private final int totalPages;
-    private final long totalItems;
+  private final int currentPage;
+  private final int totalPages;
+  private final long totalItems;
 }
--- be15_DevEagles_BE/src/test/java/com/deveagles/be15_deveagles_be/Be15DevEaglesBeApplicationTests.java
+++ be15_DevEagles_BE/src/test/java/com/deveagles/be15_deveagles_be/Be15DevEaglesBeApplicationTests.java
@@ -1,9 +1,21 @@
 package com.deveagles.be15_deveagles_be;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+    classes = Be15DevEaglesBeApplicationTests.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration(
+    exclude = {
+      DataSourceAutoConfiguration.class,
+      HibernateJpaAutoConfiguration.class,
+      SecurityAutoConfiguration.class
+    })
 class Be15DevEaglesBeApplicationTests {
 
   @Test
