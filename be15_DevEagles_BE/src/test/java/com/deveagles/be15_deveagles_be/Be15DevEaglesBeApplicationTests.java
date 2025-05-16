62f39b39bc3735c242837737e86484e8f4c6839d
Yangha Park <64etuor@gmail.com>
2025-05-16 19:10:46 +0900

    chore:의존성 문제 주석 처리, 테스트 configuration 수정(향후 삭제 필요), 기본 application.yml 임시 설정

--- be15_DevEagles_BE/src/test/java/com/deveagles/be15_deveagles_be/Be15DevEaglesBeApplicationTests.java
+++ be15_DevEagles_BE/src/test/java/com/deveagles/be15_deveagles_be/Be15DevEaglesBeApplicationTests.java
@@ -6,15 +6,29 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(
     classes = Be15DevEaglesBeApplicationTests.class,
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+// 의존성 사용하게 되면 아래 코드 삭제
 @EnableAutoConfiguration(
     exclude = {
       DataSourceAutoConfiguration.class,
       HibernateJpaAutoConfiguration.class,
       SecurityAutoConfiguration.class
+    })
+@TestPropertySource(
+    properties = {
+      "spring.cloud.aws.region.static=us-east-1",
+      "spring.cloud.aws.s3.enabled=false",
+      "spring.cloud.aws.dynamodb.enabled=false",
+      "spring.cloud.aws.ses.enabled=false",
+      "spring.cloud.aws.sqs.enabled=false",
+      "spring.cloud.aws.sns.enabled=false",
+      "spring.cloud.aws.credentials.access-key=test",
+      "spring.cloud.aws.credentials.secret-key=test"
     })
 class Be15DevEaglesBeApplicationTests {
 
