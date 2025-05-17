7e945384d84df35dc971f35079e16421ee9f646f
Yangha Park <64etuor@gmail.com>
2025-05-17 18:16:18 +0900

    feat: mongodb config 설정

---
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/common/config/MongoDBConfig.java
@@ -0,0 +1,25 @@
+package com.deveagles.be15_deveagles_be.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "com.deveagles.be15_deveagles_be.features.chat")
+public class MongoDBConfig {
+
+  @Bean
+  public MongoTemplate mongoTemplate(
+      MongoDatabaseFactory mongoDatabaseFactory, MongoMappingContext context) {
+    MappingMongoConverter converter =
+        new MappingMongoConverter(new DefaultDbRefResolver(mongoDatabaseFactory), context);
+    converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    return new MongoTemplate(mongoDatabaseFactory, converter);
+  }
+}
