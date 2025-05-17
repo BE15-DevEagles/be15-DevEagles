852f26ce75154d4ece1f9554f7b86f231fa3a123
Yangha Park <64etuor@gmail.com>
2025-05-17 16:03:15 +0900

    feat:DEV-8 #comment CQRS 구조 생성 및 도메인 entity 구현,의존성 추가

---
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/chat/command/domain/entity/ChatRoom.java
@@ -0,0 +1,57 @@
+package com.deveagles.be15_deveagles_be.features.chat.command.domain.entity;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "chatroom")
+@Getter
+@Builder
+public class ChatRoom {
+
+  @Id private String id;
+
+  private String teamId;
+
+  private String name;
+
+  @Builder.Default private boolean isDefault = false;
+
+  private ChatRoomType type;
+
+  private LastMessageInfo lastMessage;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime deletedAt;
+
+  @Getter
+  @Builder
+  public static class LastMessageInfo {
+    private String id;
+    private String content;
+    private String senderId;
+    private String senderName;
+    private LocalDateTime sentAt;
+  }
+
+  public enum ChatRoomType {
+    GROUP,
+    DIRECT,
+    TEAM
+  }
+
+  public boolean isDeleted() {
+    return deletedAt != null;
+  }
+
+  public void delete() {
+    this.deletedAt = LocalDateTime.now();
+  }
+
+  public void updateLastMessage(LastMessageInfo lastMessage) {
+    this.lastMessage = lastMessage;
+  }
+}
---
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/chat/command/domain/entity/UserThumbnail.java
@@ -0,0 +1,26 @@
+package com.deveagles.be15_deveagles_be.features.chat.command.domain.entity;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user_thumbnail")
+@Getter
+@Builder
+public class UserThumbnail {
+
+  @Id private String thumbnailId;
+
+  private String url;
+
+  private String userId;
+
+  private Map<String, Object> metadata;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime modifiedAt;
+}
