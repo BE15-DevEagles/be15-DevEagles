099a5353100685129c5f9b2298f36b30c36ec843
Yangha Park <64etuor@gmail.com>
2025-05-17 18:41:27 +0900

    feat:DEV-16 #comment 몽고DB 인프라 레포지토리 인터페이스 추가

---
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/chat/command/infrastructure/repository/SpringDataMongoChatMessageRepository.java
@@ -0,0 +1,27 @@
+package com.deveagles.be15_deveagles_be.features.chat.command.infrastructure.repository;
+
+import com.deveagles.be15_deveagles_be.features.chat.command.domain.aggregate.ChatMessage;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpringDataMongoChatMessageRepository extends MongoRepository<ChatMessage, String> {
+  List<ChatMessage> findByChatroomId(String chatroomId);
+
+  List<ChatMessage> findByChatroomIdAndDeletedAtIsNull(String chatroomId);
+
+  List<ChatMessage> findByChatroomIdAndCreatedAtBeforeAndDeletedAtIsNull(
+      String chatroomId, LocalDateTime before, Pageable pageable);
+
+  List<ChatMessage> findByChatroomIdAndCreatedAtAfterAndDeletedAtIsNull(
+      String chatroomId, LocalDateTime after, Pageable pageable);
+
+  Page<ChatMessage> findByChatroomIdAndDeletedAtIsNull(String chatroomId, Pageable pageable);
+
+  long countByChatroomIdAndCreatedAtAfterAndDeletedAtIsNull(
+      String chatroomId, LocalDateTime timestamp);
+}
