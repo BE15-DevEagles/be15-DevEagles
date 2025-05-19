5b1a6c64cd5e9bdbbf83ebb6fbb284d074ae2a76
Yangha Park <64etuor@gmail.com>
2025-05-19 17:09:57 +0900

    chore:pre-commit 테스트

--- be15_DevEagles_FE/src/features/chat/components/ChatRoomListView.vue
+++ be15_DevEagles_FE/src/features/chat/components/ChatRoomListView.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <h1>           ㅁㄴㅇㄻㄴㅇㅇㅁㄴㅇㄹㄴㅇㄹㄹ 알망ㄹ말아아ㅏ</h1>
+  <h1>ㅁㄴㅇㄻㄴㅇㅇㅁㄴㅇㄹㄴㅇㄹㄹ 알망ㄹ말아아ㅏ</h1>
 
-                      <span>             하하항허ㅏ허하 </span>
-  <input  type="button"                name="하하" />
+  <span> 하하항허ㅏ허하 </span>
+  <input type="button" name="하하" />
 </template>
 
 <style scoped></style>
