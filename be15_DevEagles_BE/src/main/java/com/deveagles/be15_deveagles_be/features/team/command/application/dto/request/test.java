8e5677a2c0599c1fd27f2fa28ceb2e54e1eac55c
gyeongmin03 <gyeongmin03@naver.com>
2025-05-19 18:03:29 +0900

    feat: DEV-58 #comment test

---
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/team/command/application/dto/request/test.java
@@ -0,0 +1,7 @@
+package com.deveagles.be15_deveagles_be.features.team.command.application.dto.request;
+
+public class test {
+  public static void main(String[] args) {
+    System.out.println("test");
+  }
+}
