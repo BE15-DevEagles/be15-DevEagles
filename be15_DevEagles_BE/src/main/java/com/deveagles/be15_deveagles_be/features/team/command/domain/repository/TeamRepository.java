685158b06984d58e07b7731581e13a87160c1c44
gyeongmin03 <gyeongmin03@naver.com>
2025-05-19 21:48:05 +0900

    feat: DEV-65 #comment 팀원 초대 기능 구현 - 팀원 초대 기능 구현 - 팀 생성 시 팀장도 TeamMember로 저장되도록 로직 추가 - Service Test Code 분리

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/team/command/domain/repository/TeamRepository.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/team/command/domain/repository/TeamRepository.java
@@ -1,6 +1,7 @@
 package com.deveagles.be15_deveagles_be.features.team.command.domain.repository;
 
 import com.deveagles.be15_deveagles_be.features.team.command.domain.aggregate.Team;
+import java.util.Optional;
 
 public interface TeamRepository {
   // 팀 생성을 위한 메서드
@@ -8,4 +9,6 @@ public interface TeamRepository {
 
   // 팀 이름 중복 예외 처리를 위한 메서드
   boolean existsByTeamName(String teamName);
+
+  Optional<Team> findById(Long teamId);
 }
