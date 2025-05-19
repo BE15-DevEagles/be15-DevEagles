31f1debc261bb83468eda6d8f8e2c6e69ec80978
Yangha Park <64etuor@gmail.com>
2025-05-19 16:03:38 +0900

    chore:pre-commit 테스트

--- be15_DevEagles_FE/src/features/chat/components/ChatRoomListView.vue
+++ be15_DevEagles_FE/src/features/chat/components/ChatRoomListView.vue
@@ -5,8 +5,8 @@
 <template>
   <h1>            알망ㄹ말아아ㅏ</h1>
 
-  <span> 하하항허ㅏ허하</span>
-  <input                 type="button" name="하하" />
-</template>
+  <span>               하하항허ㅏ허하      </span>
+  <input                 type="button"                  name="하하" />
+               </template>
 
 <style scoped></style>
