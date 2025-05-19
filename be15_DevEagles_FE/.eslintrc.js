f3ea6c6894543ac2ab825ff30cba70b5cd277979
Yangha Park <64etuor@gmail.com>
2025-05-19 15:15:53 +0900

    chore:husky, lintstaged 설정, vue js 최적화

--- be15_DevEagles_FE/.eslintrc.js
+++ be15_DevEagles_FE/.eslintrc.js
@@ -7,12 +7,22 @@ export default {
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 2022,
     sourceType: "module",
   },
   rules: {
     "vue/multi-word-component-names": "off",
+    "vue/html-self-closing": ["warn", {
+      html: { void: "always", normal: "always", component: "always" }
+    }],
+    "vue/max-attributes-per-line": ["warn", {
+      singleline: 3,
+      multiline: 1
+    }],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": "warn",
+    "semi": ["warn", "always"],
+    "quotes": ["warn", "single"]
   },
 };
