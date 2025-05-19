97dbd4717b7a750933c365ec3baae662681687e9
nineeko <gkxm1013@naver.com>
2025-05-19 16:49:14 +0900

    feature:DEV-69 #comment todolist 수정 서비스 구현, 테스트 코드 작성

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/todolist/command/application/service/TodoService.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/todolist/command/application/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.deveagles.be15_deveagles_be.features.todolist.command.application.service;
 
 import com.deveagles.be15_deveagles_be.features.todolist.command.application.dto.request.CreateTodoRequest;
+import com.deveagles.be15_deveagles_be.features.todolist.command.application.dto.request.UpdateTodoRequest;
 import com.deveagles.be15_deveagles_be.features.todolist.command.application.dto.response.TodoResponse;
 import com.deveagles.be15_deveagles_be.features.todolist.command.domain.aggregate.Todo;
 import com.deveagles.be15_deveagles_be.features.todolist.command.domain.repository.TodoRepository;
@@ -56,8 +57,24 @@ public class TodoService {
             .findById(todoId)
             .orElseThrow(() -> new TodoNotFoundException(TodoErrorCode.TODO_NOT_FOUND));
 
-    todo.complete(); // completedAt = now()
+    todo.complete();
 
     return TodoResponse.builder().todoId(todo.getTodoId()).message("할 일이 완료 처리되었습니다.").build();
+  }
+
+  @Transactional
+  public TodoResponse updateTodo(Long todoId, UpdateTodoRequest request) {
+    Todo todo =
+        todoRepository
+            .findById(todoId)
+            .orElseThrow(() -> new TodoNotFoundException(TodoErrorCode.TODO_NOT_FOUND));
+
+    if (request.getStartDate().isAfter(request.getDueDate())) {
+      throw new InvalidTodoDateException(TodoErrorCode.INVALID_TODO_DATE);
+    }
+
+    todo.updateContent(request.getContent(), request.getStartDate(), request.getDueDate());
+
+    return TodoResponse.builder().todoId(todo.getTodoId()).message("할 일이 수정되었습니다.").build();
   }
 }
