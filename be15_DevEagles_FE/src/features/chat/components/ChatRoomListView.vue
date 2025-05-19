65182377b544968463f0541776f3a6d006ed1771
Yangha Park <64etuor@gmail.com>
2025-05-19 15:12:17 +0900

    chore: husky lint-staged 테스트

---
+++ be15_DevEagles_FE/src/features/chat/components/ChatRoomListView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>알망ㄹ말아아ㅏ           </h1>
+
+  <span>      하하항허ㅏ허하</span>
+  <input type="button"          name="하하">
+</template>
+
+<style scoped>
+
+</style>
