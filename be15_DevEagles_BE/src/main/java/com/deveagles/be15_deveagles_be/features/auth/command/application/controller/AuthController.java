13110fb262ed230c05708e89ff4e9b786ea058e0
nare20027 <nare20027@gmail.com>
2025-05-19 19:58:53 +0900

    refactor:DEV-89 로그인 시 refreshToken 쿠키 저장

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/auth/command/application/controller/AuthController.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/auth/command/application/controller/AuthController.java
@@ -4,13 +4,15 @@ import com.deveagles.be15_deveagles_be.common.dto.ApiResponse;
 import com.deveagles.be15_deveagles_be.features.auth.command.application.dto.request.LoginRequest;
 import com.deveagles.be15_deveagles_be.features.auth.command.application.dto.response.TokenResponse;
 import com.deveagles.be15_deveagles_be.features.auth.command.application.service.AuthService;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -21,8 +23,27 @@ public class AuthController {
   @PostMapping("/login")
   public ResponseEntity<ApiResponse<TokenResponse>> login(@RequestBody LoginRequest request) {
 
-    TokenResponse token = authService.login(request);
+    TokenResponse response = authService.login(request);
 
-    return ResponseEntity.ok(ApiResponse.success(token));
+    return buildTokenResponse(response);
+  }
+
+  private ResponseEntity<ApiResponse<TokenResponse>> buildTokenResponse(TokenResponse response) {
+
+    ResponseCookie cookie = createRefreshTokenCookie(response.getRefreshToken());
+
+    return ResponseEntity.ok()
+        .header(HttpHeaders.SET_COOKIE, cookie.toString())
+        .body(ApiResponse.success(response));
+  }
+
+  private ResponseCookie createRefreshTokenCookie(String refreshToken) {
+
+    return ResponseCookie.from("refreshToken", refreshToken)
+        .httpOnly(true)
+        .path("/")
+        .maxAge(Duration.ofDays(7))
+        .sameSite("Strict")
+        .build();
   }
 }
