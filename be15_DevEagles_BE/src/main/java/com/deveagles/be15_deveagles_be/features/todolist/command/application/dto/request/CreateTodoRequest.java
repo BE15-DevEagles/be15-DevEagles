a49665b19e4c77b91f9cd8dc9fa1cce5bbe544bc
nineeko <gkxm1013@naver.com>
2025-05-19 15:37:01 +0900

    feat:DEV-54 #comment todo 작성 서비스 구현, yml 세팅

---
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/todolist/command/application/dto/request/CreateTodoRequest.java
@@ -0,0 +1,18 @@
+package com.deveagles.be15_deveagles_be.features.todolist.command.application.dto.request;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateTodoRequest {
+  private Long teamId;
+  private String content;
+  private LocalDateTime startDate;
+  private LocalDateTime dueDate;
+}
---
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/todolist/exception/TodoErrorCode.java
@@ -0,0 +1,17 @@
+package com.deveagles.be15_deveagles_be.features.todolist.exception;
+
+import com.deveagles.be15_deveagles_be.common.exception.ErrorCodeType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public enum TodoErrorCode implements ErrorCodeType {
+  INVALID_TODO_DATE("50001", "시작일은 마감일보다 빠를 수 없습니다.", HttpStatus.BAD_REQUEST);
+
+  private final String code;
+  private final String message;
+  private final HttpStatus httpStatus;
+}
