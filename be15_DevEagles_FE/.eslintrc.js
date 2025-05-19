86413fbe2c1efe8c749074c9950376af0d8b5c91
Yangha Park <64etuor@gmail.com>
2025-05-19 15:53:47 +0900

    chore:pre-commit 테스트

--- be15_DevEagles_FE/.eslintrc.js
+++ be15_DevEagles_FE/.eslintrc.js
@@ -5,24 +5,34 @@ export default {
     node: true,
     es2022: true,
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended'],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: "module",
+    sourceType: 'module',
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
+  plugins: ['vue'],
   rules: {
-    "vue/multi-word-component-names": "off",
-    "vue/html-self-closing": ["warn", {
-      html: { void: "always", normal: "always", component: "always" }
-    }],
-    "vue/max-attributes-per-line": ["warn", {
-      singleline: 3,
-      multiline: 1
-    }],
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-unused-vars": "warn",
-    "semi": ["warn", "always"],
-    "quotes": ["warn", "single"]
+    'vue/multi-word-component-names': 'off',
+    'vue/html-self-closing': [
+      'warn',
+      {
+        html: { void: 'always', normal: 'always', component: 'always' },
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      'warn',
+      {
+        singleline: 3,
+        multiline: 1,
+      },
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'warn',
+    semi: ['warn', 'always'],
+    quotes: ['warn', 'single'],
   },
 };
