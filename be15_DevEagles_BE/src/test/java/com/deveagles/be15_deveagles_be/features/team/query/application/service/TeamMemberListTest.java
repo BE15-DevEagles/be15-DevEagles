82ca85a01dbcbeecf043830cc39e57c1c0e371e3
gyeongmin03 <gyeongmin03@naver.com>
2025-05-21 16:17:52 +0900

    feat: DEV-150 #comment 팀 정보 조회 기능 구현 -Test Code 오타 수정

--- be15_DevEagles_BE/src/test/java/com/deveagles/be15_deveagles_be/features/team/query/application/service/TeamMemberListTest.java
+++ be15_DevEagles_BE/src/test/java/com/deveagles/be15_deveagles_be/features/team/query/application/service/TeamMemberListTest.java
@@ -16,7 +16,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class TeamQueryServiceImplTest {
+class TeamMemberListTest {
 
   private TeamMapper teamMapper;
   private TeamMemberRepository teamMemberRepository;
