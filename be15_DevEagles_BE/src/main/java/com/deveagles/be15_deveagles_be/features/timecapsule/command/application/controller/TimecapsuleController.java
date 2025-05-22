3aa4b20ae7810f06494fe49d447ab60d0ced7b16
Jayboo816 <jayboo8160@gmail.com>
2025-05-22 19:08:45 +0900

    Feature/dev 168 be 타입캡슐 출력

--- be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/timecapsule/command/application/controller/TimecapsuleController.java
+++ be15_DevEagles_BE/src/main/java/com/deveagles/be15_deveagles_be/features/timecapsule/command/application/controller/TimecapsuleController.java
@@ -4,7 +4,9 @@ import com.deveagles.be15_deveagles_be.common.dto.ApiResponse;
 import com.deveagles.be15_deveagles_be.features.auth.command.application.model.CustomUser;
 import com.deveagles.be15_deveagles_be.features.timecapsule.command.application.dto.request.CreateTimecapsuleRequest;
 import com.deveagles.be15_deveagles_be.features.timecapsule.command.application.service.TimecapsuleService;
+import com.deveagles.be15_deveagles_be.features.timecapsule.command.domain.aggregate.Timecapsule;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,5 +26,12 @@ public class TimecapsuleController {
       @AuthenticationPrincipal CustomUser user) {
     timecapsuleService.createTimecapsule(request, user.getUserId());
     return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(null));
+  }
+
+  @GetMapping("/team/{teamId}/open")
+  public ResponseEntity<ApiResponse<List<Timecapsule>>> openTeamTimecapsules(
+      @PathVariable Long teamId, @AuthenticationPrincipal CustomUser user) {
+    List<Timecapsule> result = timecapsuleService.openTeamTimecapsules(teamId, user.getUserId());
+    return ResponseEntity.ok(ApiResponse.success(result));
   }
 }
